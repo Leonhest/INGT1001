c235a9e4d3788ea7ee6c48da002de071a8506fe2
Leonhest <leon.hesthaug@gmail.com>
2021-10-19 14:32:21 +0200

    Co-authored-by: TrymNOHG <TrymNOHG@users.noreply.github.com>

--- bil_race/main.py
+++ bil_race/main.py
@@ -12,82 +12,38 @@ import time
 import random
 
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-
-# Create your objects here.
 ev3 = EV3Brick()
-
-
-# Write your program here.
 
 right_motor = Motor(Port.A)
 left_motor = Motor(Port.D)
 robot = DriveBase (right_motor, left_motor, wheel_diameter = 56, axle_track = 114)
+back_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 
 cs1 = ColorSensor(Port.S1)
 cs2 = ColorSensor(Port.S4)
 us = UltrasonicSensor(Port.S3)
 
-
-
-
 kp = 2
 
 speed = 150
 
-
-program_on = True
-robo_on = True
-
-
+ev3.speaker.say("Start")
+ref_limit1 = cs1.reflection()
+ref_limit2 = cs2.reflection()
 
 
-while program_on == True:
-    if ev3.buttons.pressed():
-        
-        ev3.speaker.say("Exercise 3")
-        ref_limit1 = cs1.reflection()
-        ref_limit2 = cs2.reflection()
-        
-        
-        
-        while robo_on == True:
-            
-            devi1 = ref_limit1 - cs1.reflection()
-            devi2 = (cs2.reflection() - ref_limit2) * 0.66
-            
-            
+while True:
+    devi1 = ref_limit1 - cs1.reflection()
+    devi2 = (cs2.reflection() - ref_limit2) * 0.66
+    
+    turn_r = kp * (devi1 + devi2)
 
-            turn_r = kp * (devi1 + devi2)
+    if devi1 > 0 and devi2 < 0:
+        robot.drive(speed, 0)
+    else:
+        robot.drive(speed, turn_r)
+        back_motor.run(abs(turn_r) * 2)
 
-            if devi1 > 0 and devi2 < 0 or devi1 < 0 and devi2 > 0:
-                robot.drive(speed, 0)
-                
-            else:
-                robot.drive(speed, turn_r)
-
-            
-            if us.distance() < 190:
-                robot.stop()
-                wait(2000)
-
-            if ev3.buttons.pressed():
-                ev3.speaker.say("We won!")
-                robo_on = False
-                program_on = False
-            
-            
-
-# Create your objects here.
-
- 
- 
- 
-
-
-
-
+    if us.distance() < 40:
+        robot.stop()
+        wait(1000)
